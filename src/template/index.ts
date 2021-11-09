0ed7045584ca8cdbf71c81d355f00af4dde42f58
Maciej Caderek <maciej.caderek@gmail.com>
2021-11-09 05:59:37 +0100

    Updated aocrunner

--- src/template/index.ts
+++ src/template/index.ts
@@ -5,14 +5,12 @@ const parseInput = (rawInput: string) => rawInput
 const solution1 = async (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  // Solution:
   return
 }
 
 const solution2 = async (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  // Solution:
   return
 }
 
@@ -20,13 +18,11 @@ run({
   part1: {
     tests: [
       // { input: ``, expected: "" },
-      // { input: ``, expected: "" },
     ],
     solution: solution1,
   },
   part2: {
     tests: [
-      // { input: ``, expected: "" },
       // { input: ``, expected: "" },
     ],
     solution: solution2,
